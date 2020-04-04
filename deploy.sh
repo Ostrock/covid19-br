c9dbe4814594f0bbdaec3fbf01a16d31cbe78056
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-04 20:35:43 -0300

    Altera run.sh e deploy.sh para incluir caso-full

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,31 @@ SCRIPT_PATH=$(dirname $(readlink -f $0))
 source $SCRIPT_PATH/base.sh
 DATASET="covid19"
 
+function upload_table_file() {
+	table=$1
+
+	log "[$table] Copying data to static file server"
+	s3cmd put data/output/${table}.csv.gz s3://dataset/$DATASET/${table}.csv.gz
+}
+
+function update_table() {
+	table=$1
+
+	# TODO: change collect-date on update command
+	log "[$table] Executing update command"
+	ssh $BRASILIO_SSH_USER@$BRASILIO_SSH_SERVER "$BRASILIO_UPDATE_COMMAND $DATASET $table"
+}
+
 log "Cleaning data path and collecting data"
 ./run.sh
 ./run-obitos.sh
 
 source $SCRIPT_PATH/.env
-for table in boletim caso obito-cartorio; do
-	log "[$table] Copying data to static file server"
-	s3cmd put data/output/${table}.csv.gz s3://dataset/$DATASET/${table}.csv.gz
-
-	log "[$table] Executing update command"
-	ssh $BRASILIO_SSH_USER@$BRASILIO_SSH_SERVER "$BRASILIO_UPDATE_COMMAND $DATASET $table"
+for table in boletim caso obito_cartorio; do
+	upload_table_file $table
+	update_table $table
 done
-# TODO: change collect-date on update command
-# TODO: generate status page for this dataset
+upload_table_file "caso-full"
 
 log "Generating file list page"
 python create_html.py dataset $DATASET $(date +"%Y-%m-%d") $SCRIPT_PATH/data/output/
